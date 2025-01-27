ced9e0085748854a837c1d35f637aa89cc30e13b
unknown <siddiqaboobakkarsiddiq.com>
2025-01-27 16:48:57 +0530

    body added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,17 +3,14 @@ import '../Assets/Home.css'
 import Nav from './Nav'
 import Carousel from './Carousel'
 import Footer from './FooterContainer'
+import Body from './Body'
+
 const Home = () => {
   return (
     <>
     <Nav/>
-    {/* <div className='container'>
-      <div className='slider'>hsdh jhsdbfjk snjkhdfjsn uihuidsnjui</div>
-      <div className='slider'>hsdh jhsdbfjk snjkhdfjsn uihuidsnjui</div>
-      <div className='slider'>hsdh jhsdbfjk snjkhdfjsn uihuidsnjui</div>
-      <div className='slider'>hsdh jhsdbfjk snjkhdfjsn uihuidsnjui</div>
-    </div> */}
     <Carousel/>
+    <Body/>
     <Footer/>
     </>
   )
