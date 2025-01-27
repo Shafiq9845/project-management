9fc2f0318b945fb5f325a4d241d54dfeb249b273
unknown <siddiqaboobakkarsiddiq.com>
2025-01-27 13:34:24 +0530

    footer added

---
+++ src/Components/FooterContainer.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from 'react'
+import { Footer } from "flowbite-react";
+const FooterContainer = () => {
+  return (
+    <>
+    <Footer container>
+      <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+      <Footer.LinkGroup>
+        <Footer.Link href="#">About</Footer.Link>
+        <Footer.Link href="#">Privacy Policy</Footer.Link>
+        <Footer.Link href="#">Licensing</Footer.Link>
+        <Footer.Link href="#">Contact</Footer.Link>
+      </Footer.LinkGroup>
+    </Footer>
+    </>
+  )
+}
+
+export default FooterContainer
--- src/Components/Routers.js
+++ src/Components/Routers.js
@@ -2,12 +2,16 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+import Nav from './Nav';
+import Carousel from './Carousel';
 import Forgotpsw from './ForgotPassword';
 const Routers = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/nav' element={<Nav/>}/>
+            <Route path='/Carousel' element={<Carousel/>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/forgot-password' element={<Forgotpsw/>}/>
         </Routes>
